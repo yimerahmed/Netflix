f4ea29f999b59421e5f8297e0590157f158a9878
yimerahmed <yimerahmed55@gmail.com>
2025-11-27 02:27:07 -0800

    header second commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,21 +2,48 @@ import React from "react";
 import netflixLogo from "../../assets/Netflix-logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "./Header.css";
+
 const Header = () => {
   return (
-    <div className="d-flex header-wrapper">
-      <div className="left-wrapper">
-        <img src={netflixLogo} alt="Netflix Logo" />
-      </div>
+    <div className="header-wrapper d-flex">
+      <div className="header-container d-flex justify-content-between align-items-center w-100">
+        {/* LEFT */}
+        <div className="left-wrapper d-flex align-items-center">
+          <ul className="icons-list-left d-flex align-items-center">
+            <li>
+              <img src={netflixLogo} alt="Netflix Logo" width={100} />
+            </li>
 
-      <div className="right-wrapper d-flex justify-content-end gap-3">
-        <SearchIcon
-          style={{ color: "white", fontSize: 28, cursor: "pointer" }}
-        />
-        <NotificationsOutlinedIcon
-          style={{ color: "white", fontSize: 28, cursor: "pointer" }}
-        />
+            {/* Hide menu on small screens */}
+            <li className="d-none d-md-block">Home</li>
+            <li className="d-none d-md-block">TV Shows</li>
+            <li className="d-none d-md-block">Movies</li>
+            <li className="d-none d-md-block">Latest</li>
+            <li className="d-none d-md-block">My List</li>
+            <li className="d-none d-lg-block">Browse by language</li>
+          </ul>
+        </div>
+
+        {/* RIGHT */}
+        <div className="right-wrapper">
+          <ul className="icons-list d-flex align-items-center">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsOutlinedIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Components/RowList.jsx
+++ src/Components/RowList.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import sample from '../assets/ekZobS8isE6mA53RAiGDG93hBxL.webp'
 const RowList = () => {
+  const [movies,setMovies]=useState({})
+  useEffect(()=>{
+    fetch("")
+    .then()
+
+  },[])
   return (
     <div className='image-wrapper'>
       <img src={sample} alt="" />
